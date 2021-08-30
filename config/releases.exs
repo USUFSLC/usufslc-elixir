40f3c40903b453c8b96da50ad0751f5787899efa
Logan Hunt <loganhunt@simponic.xyz>
2021-08-30 07:58:56 -0600

    Add docker file

--- config/releases.exs
+++ config/releases.exs
@@ -2,7 +2,7 @@
 # from environment variables. You can also hardcode secrets,
 # although such is generally not recommended and you have to
 # remember to add this file to your .gitignore.
-use Mix.Config
+import Config
 
 database_url =
   System.get_env("DATABASE_URL") ||
@@ -24,6 +24,7 @@ secret_key_base =
     """
 
 config :fslc, FslcWeb.Endpoint,
+  server: true,
   http: [
     port: String.to_integer(System.get_env("PORT") || "4000"),
     transport_options: [socket_opts: [:inet6]]
--- start_dev_server.sh
+++ start_dev_server.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-export POSTGRES_USER="postgres"
-export POSTGRES_PASS="postgres"
-export POSTGRES_DB="fslc_dev"
-export POSTGRES_HOSTNAME="localhost"
-export HTTP_PORT="4000"
+export $(cat .env | xargs)
 
 mix phx.server
